97b6884bb05de94ed9367b92b3c890964264bf36
Robert Hart <hart@g.harvard.edu>
2020-01-08 14:28:26 -0500

    comment code.

--- SC_lab/Projects/Stepper_driver/Code/Arduino_SAM/itsy_bitsy_contoller_square/itsy_bitsy_contoller_square.ino
+++ SC_lab/Projects/Stepper_driver/Code/Arduino_SAM/itsy_bitsy_contoller_square/itsy_bitsy_contoller_square.ino
@@ -1,24 +1,20 @@
 
 int delta_x = 15;
 
-
-
-
-
 void setup() {
    Serial1.begin(230400);
    Serial.begin(0);
 }
 
-void step(int dx, int dy){        
+void step(int dx, int dy){
   int delta_1;
   int delta_2;
   uint8_t byte_1;
   uint8_t byte_2;
   uint8_t step_1_dir;
   uint8_t step_2_dir;
-  
-  delta_1=dx+dy;
+
+  delta_1=dx+dy;        //defined this way for h-bot
   delta_2 =dx-dy;
 
   if (delta_1 >= 0) step_1_dir = 1;
@@ -26,19 +22,19 @@ void step(int dx, int dy){
 
   if (delta_2 >= 0) step_2_dir = 1;
   else step_2_dir = 0;
-  
-  byte_1 = 128 * step_1_dir + 16 + abs(delta_1);
-  byte_2 = 128 * step_2_dir+ 2*16 + abs(delta_2);
+
+  byte_1 = 128 * step_1_dir + 16 + abs(delta_1);    //this packs direction, address and step number into one byte. address = 1
+  byte_2 = 128 * step_2_dir+ 2*16 + abs(delta_2);   //this packs direction, address and step number into one byte. address = 2
   Serial1.write(byte_1);
-  Serial1.write(byte_2); 
+  Serial1.write(byte_2);
 }
 
 
 void loop() {
 
-  
+
   int step_delay = 50;
-  
+
   for (int i=0; i<50; i++){
   step(5,0);
   delay(step_delay);
@@ -58,5 +54,5 @@ void loop() {
   step(0,-5);
   delay(step_delay);
   }
-  
+
   }
--- SC_lab/Projects/Stepper_driver/Code/Arduino_SAM/samd11_stepper_3/samd11_stepper_3.ino
+++ SC_lab/Projects/Stepper_driver/Code/Arduino_SAM/samd11_stepper_3/samd11_stepper_3.ino
@@ -1,19 +1,26 @@
 /*
-Inplement identity and direction encoded in USART byte.
+Jan 7, 2020.  Rob Hart.  samd11_stepper_3
+Program to drive stepper in response to one byte transferred into USART NO. 1.
+MSB(7): direction.
+6-4: address.
+3-0: number of steps to take.
+In this program, step time is fixed.
+
 */
-int Aplus = 15;
-int Aminus = 14;
-int Bplus = 8;
-int Bminus = 5;
-int LED_G = 4;
+int Aplus = 15;   //corresponds to h-bridge input pin +
+int Aminus = 14;   //corresponds to h-bridge input pin -
+int Bplus = 8;   //corresponds to h-bridge input pin +
+int Bminus = 5;   //corresponds to h-bridge input pin -
+int LED_G = 4;    //LEDs are attached to pins to give a visual indication of direction and step
 int LED_R = 2;
 
 int step_num = 1;
-uint16_t step_count = 0;
-uint8_t pulse_count = 0;
-uint8_t rec_byte = 0;
-boolean direction = true;
-uint8_t address = 2;
+uint16_t step_count = 0;      //number of steps since program began.
+uint8_t pulse_count = 0;     //The variable with range 0-3 that determines which motor state to
+uint8_t rec_byte = 0;         //The byte received from USART
+bool direction = true;        //True corresponds to incrementing pulse count.
+uint8_t address = 2;      //This value is unique to a given board. Ranges 0-7.
+//uint8_t address = 1;      //This value is unique to a given board. Ranges 0-7.
 
 void setup() {
   Serial1.begin(230400);
@@ -24,9 +31,11 @@ void setup() {
   pinMode(Bminus, OUTPUT);
   pinMode(LED_G, OUTPUT);
   pinMode(LED_R, OUTPUT);
-  
-}
 
+}
+/*
+The following functions energise the windings, in order defined as positive stepping
+*/
 void pulse_0(){
   digitalWrite(Aplus,HIGH);
   digitalWrite(Bplus,HIGH);
@@ -46,7 +55,9 @@ void pulse_3(){
   digitalWrite(Aplus,HIGH);
   digitalWrite(Bminus,HIGH);
 }
-
+/*
+The following function turns off all windings.
+*/
 void all_off() {
   digitalWrite(Aplus,LOW);
   digitalWrite(Aminus,LOW);
@@ -55,48 +66,48 @@ void all_off() {
 }
 
 void loop() {
-  
-  while (!Serial1.available()){
+
+  while (!Serial1.available()){       //wait until serial is available.
   }
-  
+
   rec_byte =  Serial1.read();
-  
-  
-  
-  if ((rec_byte & 0b01110000) >> 4 == address){     //if I detect my address step motor.
-    
-    digitalWrite(LED_G, HIGH);
-    
-    direction = rec_byte & 0b10000000;
-    if (direction) digitalWrite(LED_R, HIGH);
+
+
+
+  if ((rec_byte & 0b01110000) >> 4 == address){     //Bitwise detection of address using three-digit mask and shifting.
+                                                    //If address is detected, step the motor. Otherwise, go back and wait for another byte.
+    digitalWrite(LED_G, HIGH);                      //Green LED indicates start of stepping.
+
+    direction = rec_byte & 0b10000000;        //set the boolean direction to be true or false using only the most significant bit.
+    if (direction) digitalWrite(LED_R, HIGH);     //use LED as a visual indicator of + direction.
     else digitalWrite(LED_R, LOW);
-    
+
     step_num = rec_byte & 0b00001111;      //strip off lower half-byte
-    
-    for(int i=0; i<step_num; i++){                  
-      
-      if (direction) step_count++;  
-      else step_count--; 
-      
-      pulse_count = step_count%4;
-      
-      SerialUSB.println(step_count);    //keep track of overall step_count,  16 bit unsigned integer.
-      
+
+    /*
+    The following loop steps the motor for 'step_num' steps.
+    */
+    for(int i=0; i<step_num; i++){
+
+      if (direction) step_count++;
+      else step_count--;
+
+      pulse_count = step_count%4;       //use the modulo function to keep track of which motor state to use.
+
+      SerialUSB.println(step_count);    //print out overall step count.
+
       all_off();
       delayMicroseconds(10);
-      
+
       if (pulse_count == 0) pulse_0();
       if (pulse_count == 1) pulse_1();
       if (pulse_count == 2) pulse_2();
-      if (pulse_count == 3) pulse_3(); 
-  
-      delayMicroseconds(5000);      //microseconds
+      if (pulse_count == 3) pulse_3();
+
+      delayMicroseconds(5000);      //microseconds.  This, together with unknown overhead, defines the step time.
     }
+    digitalWrite(LED_G, LOW);    //done with stepping.
       }
-       
-  else digitalWrite(LED_G, LOW);    //in the case that I don't wee my address.
- 
-    
-  
-  
+
+
  }
