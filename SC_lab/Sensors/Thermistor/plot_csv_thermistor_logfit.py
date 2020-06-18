fb59536815af1c228cf88e75b0c9c753b9d23bb4
Robert Hart <hart@g.harvard.edu>
2020-06-18 15:48:43 -0400

    add matl. to trap project

---
+++ SC_lab/Sensors/Thermistor/plot_csv_thermistor_logfit.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#data_file = "/Users/roberthart/Documents/Python/example.csv" 
+data_file = "/Users/roberthart/Documents/Python/thermistor_10K_NTC.csv" 
+
+data_set = np.loadtxt(data_file, delimiter=',')
+T = data_set[:,0]
+R = data_set[:,1]
+
+a1=3.354e-3
+b1=2.5698e-4
+rref = 10000
+
+Tmod = 1/(a1 + b1*np.log(R/rref))-273.15
+
+plt.plot(R, T, 'ro')
+plt.plot(R, Tmod)
+plt.xlabel('R (Ohm)')
+plt.ylabel('T (degree Celcius)')
+plt.title('Thermistor Data for 10K NTC')
+plt.show
