a81899dd83406cd65fb33289e3c192fb49ad35de
Robert Hart <hart@g.harvard.edu>
2020-07-20 17:02:45 -0400

    networking adds

--- SC_lab/S-12machine/serial_step_setpoint3/step_to_target.ino
+++ SC_lab/S-12machine/serial_step_setpoint3/step_to_target.ino
@@ -7,7 +7,7 @@ void step_to_target(int target1, int target2){
   int step_dir1 = 1;                                //The step_dir variables can be 1,-1,or 0.
   int step_dir2 = 1;
   
-  while( !( (step_dir1 == 0)&&(step_dir1 == 0) ) ){       //Keep looping until both targets are reached.
+  while( !( (step_dir1 == 0)&&(step_dir2 == 0) ) ){       //Keep looping until both targets are reached.
     if (target1 - step_count1 > 0) step_dir1 = 1;         //This portion updates motor 1. First check to see which way to go.
     else if (target1 - step_count1 < 0 ) step_dir1 = -1;    //
     else step_dir1 = 0;
